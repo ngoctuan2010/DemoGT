414bdc4e43747cda97ada80fadec5f000958b439
2051052145tuan@ou.edu.vn <2051052145tuan@ou.edu.vn>
2023-02-17 14:32:09 +0700

    fix feature 1

--- Testf/src/main/java/com/thinh/testf/PrimaryController.java
+++ Testf/src/main/java/com/thinh/testf/PrimaryController.java
@@ -8,7 +8,7 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         int a = 20;
-        if(a > 5)
+        if(a > 5 && a % 2 == 0)
             App.setRoot("secondary");
     }
 }
